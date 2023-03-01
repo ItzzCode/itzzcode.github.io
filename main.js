6e3214ee3391bf36ef4444405d36adb3363bbed1
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2023-03-01 14:01:47 -0500

    get jekyll'd bozo

--- main.js
+++
@@ -1,4 +0,0 @@
-var extern = document.getElementsByTagName("link")[0].import;
-window.addEventListener("load", function() {
-	document.getElementsByTagName("html")[0].replaceChild(extern.getElementsByTagName("topbar")[0], document.getElementsByTagName("topbar")[0]);
-  }, false);
